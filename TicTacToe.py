1840556dd2d1cbeef1feacddb79637eb71f0004e
Kyle-Hannon <35232249+Kyle-Hannon@users.noreply.github.com>
2018-11-29 13:19:05 -0800

    Python Tic-Tac-Toe Game

---
+++ TicTacToe.py
@@ -0,0 +1,103 @@
+# Tic-Tac-Toe
+
+board = [2, 2, 2,
+         2, 2, 2,
+         2, 2, 2]
+o = 0
+x = 1
+current_player = int(1)
+takenspots = 0
+empty = 2
+WON = False
+
+
+# Checks to see if the selected spot is taken
+def check_spot(z):
+    if board[z] == 1 or 2:
+        return True
+    else:
+        return False
+
+
+# Checks to see if someone won
+def check_won():
+    if board[0] and board[1] and board[2] == 0 or 1:
+        WON = True
+    elif board[0] and board[3] and board[6] == 0 or 1:
+        WON = True
+    elif board[0] and board[4] and board[8] == 0 or 1:
+        WON = True
+    elif board[1] and board[4] and board[7] == 0 or 1:
+        WON = True
+    elif board[2] and board[5] and board[8] == 0 or 1:
+        WON = True
+    elif board[3] and board[4] and board[5] == 0 or 1:
+        WON = True
+    elif board[6] and board[7] and board[8] == 0 or 1:
+        WON = True
+    else:
+        WON = False
+
+
+def player_check():
+    if current_player % 2 == 0:
+        move = int(input("Choose your position"))
+        make_move(move)
+    elif current_player % 2 == 1:
+        move = int(input("Choose your position"))
+        make_move(move)
+
+
+def make_move(y):
+    if check_spot(y) is False:
+        print("You can't make that move")
+    else:
+        if current_player % 2 == 0:
+            board[y] = 1
+        else:
+            board[y] = 0
+
+
+def available_move():
+    for spot in board:
+        if board[spot] == 0 or 1:
+            takenspots += 1
+
+
+def draw_board():
+    print(board)
+
+
+def play_game():
+    print("Beginning a game of TicTacToe")
+    if WON is False:
+        draw_board()
+        player_check()
+        check_won()
+
+
+play_game()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
